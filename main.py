1d6a8420358080272984a7f27c944401e202e294
Xander Haijen <xanderhaijen@uantwerpen.be>
2026-05-11 20:33:11 +0200

    Revert

--- main.py
+++ main.py
@@ -7,10 +7,13 @@ st.set_page_config(page_title="RefPlan 2026", layout="wide")
 st.title("Tournament Referee Portal")
 
 # 1. Connection to Google Sheets
-# url = "https://docs.google.com/spreadsheets/d/1ZxbRCWrZ5BVI2JjBZlj8B73v2OAHkRkUtYvjSNAp9Ec/edit?usp=sharing"
+# Replace 'YOUR_SHEET_URL_HERE' with the link you copied
+url = "https://docs.google.com/spreadsheets/d/1V4IO4YVHkWUF-IyVLM1bGlTCCCvXigJ4/edit?usp=sharing&ouid=116787883560249891832&rtpof=true&sd=true"
 conn = st.connection("gsheets", type=GSheetsConnection)
+
 # Fetch data
-df = conn.read(spreadsheet="Toernooiplanning", worksheet="Games")
+df = conn.read(spreadsheet=url, worksheet="Planning")
+
 # 2. Sidebar Navigation
 menu = st.sidebar.radio("Navigation", ["My Schedule", "Full Tournament Overview"])
 
